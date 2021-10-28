871429661949d1f6677809daa232c00fac254067
Monique Rio <mrio@umich.edu>
2021-10-28 10:04:32 -0400

    added some more specs to make sure errors are being handled

--- lib/expire_passwords.rb
+++ lib/expire_passwords.rb
@@ -1,26 +1,35 @@
 require 'alma_rest_client'
+require 'logger'
 
 class PasswordExpirer
-  def initialize(users, client = AlmaRestClient.client)
+  def initialize(users, logger=Logger.new(STDOUT), client = AlmaRestClient.client)
     @users = users
     @client = client
+    @logger = logger
   end
   def run
+    error_count = 0
+    @logger.info("Started Expiring Student Worker Account Passwords")
     #log started password expirer
     @users.each do |uniqname|
       response = @client.get("/users/#{uniqname}")
       if response.code != 200
-        #log error
+        @logger.error("Unable to get info about user: #{uniqname}")
+        error_count = error_count+1
         next
       end
       body = response.parsed_response
       body["force_password_change"] = "TRUE"
       new_response = @client.put("/users/#{uniqname}", body: body.to_json)
       if new_response.code != 200
-        #log error
+        @logger.error("Unable to update user: #{uniqname}")
+        error_count = error_count+1
       else
-        #log success
+        @logger.info("Forced password change for: #{uniqname}")
       end
     end
+    @logger.info("Finished expiring student accounts.\nNumber of errors: #{error_count}")
+    { number_of_errors: error_count }
+    
   end
 end
--- spec/expire_passwords_spec.rb
+++ spec/expire_passwords_spec.rb
@@ -1,15 +1,39 @@
 require "spec_helper"
 
 RSpec.describe PasswordExpirer do
-  it "forces a password change" do
-    user_json = fixture('user.json')
-    put_body = JSON.parse(user_json)
-    put_body["force_password_change"] = "TRUE"
-    user =  "mlibrary.acct.testing1@gmail.com"
+  before(:each) do
+    @user_json = fixture('user.json')
+    @put_body = JSON.parse(@user_json)
+    @put_body["force_password_change"] = "TRUE"
+    @user =  "mlibrary.acct.testing1@gmail.com"
+    @logger = instance_double(Logger, info: nil, error: nil)
 
-    get_stub = stub_alma_get_request(url:"users/#{user}", output: user_json)    
-    put_stub = stub_alma_put_request(url:"users/#{user}", input: put_body.to_json)
-    described_class.new([user]).run
+  end
+  subject do
+    described_class.new([@user], @logger).run
+  end
+  it "forces a password change" do
+    get_stub = stub_alma_get_request(url:"users/#{@user}", output: @user_json)    
+    put_stub = stub_alma_put_request(url:"users/#{@user}", input: @put_body.to_json)
+
+    expect(@logger).not_to receive(:error)
+    expect(subject[:number_of_errors]).to eq(0)
+    expect(get_stub).to have_been_requested
+    expect(put_stub).to have_been_requested
+  end
+  it "logs an error on failed get and returns proper number of errors" do
+    get_stub = stub_alma_get_request(url:"users/#{@user}", output: nil, status: 500)    
+    put_stub = stub_alma_put_request(url:"users/#{@user}", input: @put_body.to_json)
+    expect(@logger).to receive(:error)
+    expect(subject[:number_of_errors]).to eq(1)
+    expect(get_stub).to have_been_requested
+    expect(put_stub).not_to have_been_requested
+  end
+  it "logs an error on failed put and returns proper number of errors" do
+    get_stub = stub_alma_get_request(url:"users/#{@user}", output: @user_json)    
+    put_stub = stub_alma_put_request(url:"users/#{@user}", input: @put_body.to_json, status: 500)
+    expect(@logger).to receive(:error)
+    expect(subject[:number_of_errors]).to eq(1)
     expect(get_stub).to have_been_requested
     expect(put_stub).to have_been_requested
   end
